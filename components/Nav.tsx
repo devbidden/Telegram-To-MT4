f0d52c456390cff6d66452f73f55629603516663
Raheem Kawojue <kawojue08@gmail.com>
2023-06-03 15:27:51 +0100

    [edit] nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -8,7 +8,7 @@ const Nav: React.FC = () => {
         <nav className="px-10 md:px-16 py-1.5 md:py-3 bg-clr-0 fixed top-0 w-full">
             <div className="flex items-center justify-between mx-auto">
                 <Image src={bot3} alt="logo-bot" width={90} height={90} />
-                <Link className="px-3 py-2 rounded-md text-lg md:text-xl text-clr-7 hover:text-clr-5 bg-clr-4 hover:bg-clr-6 trans font-medium tracking-wider"
+                <Link className="px-3 py-2 rounded-md md:text-lg text-clr-7 hover:text-clr-5 bg-clr-4 hover:bg-clr-6 trans font-medium tracking-wider mt-2"
                 href="http://t.me/devbidden" target="_blank">
                     Contact Developer
                 </Link>
