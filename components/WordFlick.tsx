e15afcde93e5768e46f26355abeae5c07a4b237e
Raheem Kawojue <kawojue08@gmail.com>
2023-06-04 15:23:58 +0100

    [edit]

--- components/WordFlick.tsx
+++ components/WordFlick.tsx
@@ -15,7 +15,7 @@ const WordFlick: React.FC<{ words: string[], styles?: string }> = ({ words, styl
         const interval = setInterval(() => {
             if (forwards) {
                 if (offset >= words[i].length) {
-                    setSkipCount(prevCount => prevCount + 1)
+                    setSkipCount((prevCount: number) => prevCount + 1)
                     if (skipCount === skipDelay) {
                         setForwards(false)
                         setSkipCount(0)
@@ -24,16 +24,16 @@ const WordFlick: React.FC<{ words: string[], styles?: string }> = ({ words, styl
             } else {
                 if (offset === 0) {
                     setForwards(true)
-                    setI(prevI => (prevI + 1) % words.length)
+                    setI((prevI: number) => (prevI + 1) % words.length)
                     setOffset(0)
                 }
             }
             const updatedPart: string = words[i].substr(0, offset)
             if (skipCount === 0) {
                 if (forwards) {
-                    setOffset(prevOffset => prevOffset + 1)
+                    setOffset((prevOffset: number) => prevOffset + 1)
                 } else {
-                    setOffset(prevOffset => prevOffset - 1)
+                    setOffset((prevOffset: number) => prevOffset - 1)
                 }
             }
             setPart(updatedPart)
