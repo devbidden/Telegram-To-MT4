017d704e45dcec5fd6851f1a11de76d9aa45edd2
Raheem Kawojue <kawojue08@gmail.com>
2023-06-04 16:34:55 +0100

    [edit] refractored

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,7 +6,8 @@ const Nav: React.FC = () => {
     return (
         <nav className="nav-bar md:px-16 md:py-3">
             <div className="flex items-center justify-between mx-auto">
-                <Image src="https://res.cloudinary.com/kawojue/image/upload/v1685891779/Signal%20Copier/bot-1_shk7k2.jpg" alt="logo-bot" width={80} height={80} />
+                <Image src="https://res.cloudinary.com/kawojue/image/upload/v1685891779/Signal%20Copier/bot-1_shk7k2.jpg"
+                alt="logo-bot" width={80} height={80} priority />
                 <Link className="px-3 py-2 rounded-md md:text-lg text-clr-7 hover:text-clr-5 bg-clr-4 hover:bg-clr-6 trans font-medium tracking-wider mt-2"
                 href="http://t.me/devbidden" target="_blank">
                     Contact Developer
