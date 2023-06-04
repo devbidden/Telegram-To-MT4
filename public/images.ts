18585b7699952555e38dad8224de27a32dbceaf0
Raheem Kawojue <kawojue08@gmail.com>
2023-06-04 16:28:10 +0100

    [edit]

--- public/images.ts
+++ public/images.ts
@@ -1,19 +1,15 @@
-import img1 from './img-1.png'
-import img2 from './img-2.png'
-import img3 from './img-3.png'
-
 const images: any[] = ([
     {
         id: 1,
-        img: img1
+        img: 'https://res.cloudinary.com/kawojue/image/upload/v1685891780/Signal%20Copier/img-3_f36qvs.png'
     },
     {
         id: 2,
-        img: img2
+        img: 'https://res.cloudinary.com/kawojue/image/upload/v1685891779/Signal%20Copier/img-2_xh5xur.png'
     },
     {
         id: 3,
-        img: img3
+        img: 'https://res.cloudinary.com/kawojue/image/upload/v1685891779/Signal%20Copier/img-1_pjx9kj.png'
     }
 ])
 
