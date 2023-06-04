df62dc7afd7f82fa776d576f83ca7de5440d2cdf
Raheem Kawojue <kawojue08@gmail.com>
2023-06-04 16:27:52 +0100

    [setup] cloud

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                port: '',
+                pathname: '/kawojue/image/upload/v1685891779/Signal%20Copier/**',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
