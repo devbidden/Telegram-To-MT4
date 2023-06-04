f1cc49e092688ef83fc6f1c78ca2b0b15cf3f997
Raheem Kawojue <kawojue08@gmail.com>
2023-06-04 12:54:42 +0100

    refractored nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,9 +5,9 @@ import bot3 from '@/public/bot-3.jpg'
 
 const Nav: React.FC = () => {
     return (
-        <nav className="px-10 md:px-16 py-1.5 md:py-3 bg-clr-0 fixed top-0 w-full">
+        <nav className="nav-bar md:px-16 md:py-3">
             <div className="flex items-center justify-between mx-auto">
-                <Image src={bot3} alt="logo-bot" width={90} height={90} />
+                <Image src={bot3} alt="logo-bot" width={80} height={80} />
                 <Link className="px-3 py-2 rounded-md md:text-lg text-clr-7 hover:text-clr-5 bg-clr-4 hover:bg-clr-6 trans font-medium tracking-wider mt-2"
                 href="http://t.me/devbidden" target="_blank">
                     Contact Developer
