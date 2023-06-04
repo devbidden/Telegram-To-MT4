201d49281f670c6e10e79836b71a56767cc45652
Raheem Kawojue <kawojue08@gmail.com>
2023-06-04 14:10:24 +0100

    [edit]

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,13 @@
 "use client"
 import Link from "next/link"
 import Image from "next/image"
-import bot3 from '@/public/bot-3.jpg'
+import bot from '@/public/bot-1.jpg'
 
 const Nav: React.FC = () => {
     return (
         <nav className="nav-bar md:px-16 md:py-3">
             <div className="flex items-center justify-between mx-auto">
-                <Image src={bot3} alt="logo-bot" width={80} height={80} />
+                <Image src={bot} alt="logo-bot" width={80} height={80} />
                 <Link className="px-3 py-2 rounded-md md:text-lg text-clr-7 hover:text-clr-5 bg-clr-4 hover:bg-clr-6 trans font-medium tracking-wider mt-2"
                 href="http://t.me/devbidden" target="_blank">
                     Contact Developer
